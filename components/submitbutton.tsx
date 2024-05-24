3a9aaa99a4a0ec07e6fc7f6ceb115336c2995b72
Santi van den Toorn <santi.vdt@gmail.com>
2024-05-24 09:07:19 +0200

    small update

--- components/submitbutton.tsx
+++ components/submitbutton.tsx
@@ -1,12 +1,12 @@
 import { useFormStatus } from 'react-dom'
 import { Button } from './ui/button'
 
-const SubmitButton = ({ normal, going }) => {
+const SubmitButton = ({ normal, pending }) => {
   const { pending } = useFormStatus()
 
   return (
     <Button type='submit' disabled={pending}>
-      {pending ? going : normal}
+      {pending ? pending : normal}
     </Button>
   )
 }
