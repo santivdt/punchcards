4f6dc98b64820530482d6d30d1b603ac15ec934c
Santi van den Toorn <santi.vdt@gmail.com>
2024-06-12 13:40:15 +0200

    final stuff for profile pic

--- app/u/profile/profile.tsx
+++ app/u/profile/profile.tsx
@@ -4,7 +4,6 @@ import SubmitButton from '@/components/submitbutton'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Tables } from '@/types/supabase'
-import { motion } from 'framer-motion'
 import { Pencil } from 'lucide-react'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
@@ -41,94 +40,89 @@ const ProfileForm = ({ userProfile }: ProfileFormProps) => {
     <>
       {userProfile && (
         <>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 3 }}
-          >
-            <form action={formAction} className='my-4'>
-              <input type='hidden' name='id' defaultValue={userProfile.id} />
-              <div className='flex flex-col items-start'>
-                <label className='flex items-center justify-center'>
-                  <input
-                    id='avatar'
-                    name='avatar'
-                    type='file'
-                    accept='image/*'
-                    className='hidden'
-                    onChange={handlePhotoChange}
-                  />
-                  <div className='relative inline-flex rounded-full'>
-                    <div className='flex cursor-pointer items-center justify-center rounded-full border border-dashed text-sm text-neutral-600 hover:border-neutral-300 dark:text-neutral-400 h-20 w-20'>
-                      <span className='relative bg-white shadow-small dark:bg-black dark:shadow-white/20 w-20 h-20 text-2xl inline-flex flex-none cursor-pointer rounded-full object-cover'>
-                        <Image
-                          className='aspect-square h-full w-full rounded-[inherit] object-cover ring-inset ring-black/10'
-                          alt='Profile picture'
-                          src={preview ?? '/placeholder.jpeg'}
-                          width='80'
-                          height='80'
-                          priority
-                        />
-                      </span>
-                    </div>
-                    <div className='absolute bottom-0 right-0 flex translate-y-0 cursor-pointer items-center justify-center rounded-full bg-neutral-50 p-2 shadow-md transition-all hover:-translate-y-0.5 hover:shadow-lg dark:bg-neutral-800'>
-                      <Pencil size={12} />
-                    </div>
-                  </div>
-                </label>
-              </div>
-              <div className='flex'>
-                <div className='mr-8'>
-                  <Label htmlFor='first_name'>First name</Label>
-                  <Input
-                    id='first_name'
-                    name='first_name'
-                    type='text'
-                    defaultValue={userProfile.first_name ?? ''}
-                  />
-                  {state?.errors?.first_name && (
-                    <p className='py-2 text-xs text-red-500'>
-                      {state.errors.first_name}
-                    </p>
-                  )}
-                </div>
-                <div>
-                  <Label htmlFor='last_name'>Last name</Label>
-                  <Input
-                    id='last_name'
-                    name='last_name'
-                    type='text'
-                    defaultValue={userProfile.last_name ?? ''}
-                  />
-                  {state?.errors?.last_name && (
-                    <p className='py-2 text-xs text-red-500'>
-                      {state.errors.last_name}
-                    </p>
-                  )}
-                </div>
-              </div>
-              <div className='w-fit mt-4'>
-                <Label htmlFor='company'>Company</Label>
-                <Input
-                  id='company'
-                  name='company'
-                  type='text'
-                  defaultValue={userProfile.company ?? ''}
+          <form action={formAction} className='my-4'>
+            <input type='hidden' name='id' defaultValue={userProfile.id} />
+            <div className='flex flex-col items-start mb-4'>
+              <label className='flex items-center justify-center'>
+                <input
+                  id='avatar'
+                  name='avatar'
+                  type='file'
+                  accept='image/*'
+                  className='hidden'
+                  onChange={handlePhotoChange}
                 />
-                {state?.errors?.company && (
+                <div className='relative inline-flex rounded-full'>
+                  <div className='flex cursor-pointer items-center justify-center rounded-full border border-dashed text-sm text-neutral-600 hover:border-neutral-300 dark:text-neutral-400 h-20 w-20'>
+                    <span className='relative bg-white shadow-small dark:bg-black dark:shadow-white/20 w-20 h-20 text-2xl inline-flex flex-none cursor-pointer rounded-full object-cover'>
+                      <Image
+                        className='aspect-square h-full w-full rounded-[inherit] object-cover ring-inset ring-black/10'
+                        alt='Profile picture'
+                        src={preview ?? '/placeholder.jpeg'}
+                        width='80'
+                        height='80'
+                        priority
+                      />
+                    </span>
+                  </div>
+                  <div className='absolute bottom-0 right-0 flex translate-y-0 cursor-pointer items-center justify-center rounded-full bg-neutral-50 p-2 shadow-md transition-all hover:-translate-y-0.5 hover:shadow-lg dark:bg-neutral-800'>
+                    <Pencil size={12} />
+                  </div>
+                </div>
+              </label>
+            </div>
+            <div className='flex'>
+              <div className='mr-8'>
+                <Label htmlFor='first_name'>First name</Label>
+                <Input
+                  id='first_name'
+                  name='first_name'
+                  type='text'
+                  defaultValue={userProfile.first_name ?? ''}
+                />
+                {state?.errors?.first_name && (
                   <p className='py-2 text-xs text-red-500'>
-                    {state.errors.company}
+                    {state.errors.first_name}
                   </p>
                 )}
               </div>
-              <p aria-live='polite' className='sr-only'>
-                {state?.message}
-              </p>
-              <div className='flex items-center gap-2 mt-4'>
-                <SubmitButton normal='Update' going='Updating...' />
+              <div>
+                <Label htmlFor='last_name'>Last name</Label>
+                <Input
+                  id='last_name'
+                  name='last_name'
+                  type='text'
+                  defaultValue={userProfile.last_name ?? ''}
+                />
+                {state?.errors?.last_name && (
+                  <p className='py-2 text-xs text-red-500'>
+                    {state.errors.last_name}
+                  </p>
+                )}
               </div>
-            </form>
-          </motion.div>
+            </div>
+            <div className='w-fit mt-4'>
+              <Label htmlFor='company'>Company</Label>
+              <Input
+                id='company'
+                name='company'
+                type='text'
+                defaultValue={userProfile.company ?? ''}
+              />
+              {state?.errors?.company && (
+                <p className='py-2 text-xs text-red-500'>
+                  {state.errors.company}
+                </p>
+              )}
+            </div>
+            <p aria-live='polite' className='sr-only'>
+              {state?.message}
+            </p>
+            <div className='flex items-center gap-2 mt-4'>
+              <SubmitButton normal='Update' going='Updating...' />
+            </div>
+          </form>
+
           <DangerZone userProfile={userProfile} />
         </>
       )}
