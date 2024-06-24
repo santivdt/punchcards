1e01fd3ab0948b6f9ea3b11f6392b0ddb36cc6a7
Santi van den Toorn <santi.vdt@gmail.com>
2024-06-24 10:36:46 +0200

    Keep trying

--- __tests__/clientspage.test.js
+++ __tests__/clientspage.test.js
@@ -1,14 +1,14 @@
-import Hero from '@/app/(website)/components/hero'
+import ClientsPage from '@/app/(loggedIn)/clients/page.tsx'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
 describe('Hero', () => {
   it('renders a heading', () => {
-    render(<Hero />)
+    render(<ClientsPage />)
 
     const heading = screen.getByRole('heading', { level: 1 })
 
     expect(heading).toBeInTheDocument()
-    expect(heading).toHaveTextContent('Manage your prepaid hours with ease')
+    expect(heading).toHaveTextContent('Clients')
   })
 })
