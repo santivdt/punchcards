6df89de0217fe2678d031c6e2ee1531e17e0dd3e
Santi van den Toorn <santi.vdt@gmail.com>
2024-07-10 10:08:02 +0200

    Refactor middleware

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,35 @@
 import { createClient } from '@/utils/supabase/middleware'
 import { NextResponse, type NextRequest } from 'next/server'
 
-//TODO find better solution ot handle unprotected pages, https://github.com/vercel/next.js/discussions/44635
 export async function middleware(request: NextRequest) {
-  const { supabase, response } = createClient(request)
-  if (request.url.includes('auth')) return response
+  const { supabase } = createClient(request)
+
   const {
     data: { user },
   } = await supabase.auth.getUser()
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/signup') &&
-    request.nextUrl.pathname !== '/' &&
-    request.nextUrl.pathname !== '/about' &&
-    request.nextUrl.pathname !== '/pricing' &&
-    request.nextUrl.pathname !== '/contact' &&
-    !request.nextUrl.pathname.startsWith('/forgot-password') &&
-    !request.nextUrl.pathname.startsWith('/reset-password')
-  ) {
+  const publicRoutes = [
+    '/',
+    '/about',
+    '/pricing',
+    '/contact',
+    '/login',
+    '/signup',
+    '/forgot-password',
+    '/reset-password',
+  ]
+
+  const isPublicRoute = publicRoutes.some(
+    (route) =>
+      request.nextUrl.pathname === route ||
+      request.nextUrl.pathname.startsWith(route)
+  )
+
+  if (!user && !isPublicRoute) {
     return NextResponse.redirect(new URL('/', request.url))
   }
-  return response
+
+  return NextResponse.next()
 }
 
 export const config = {
