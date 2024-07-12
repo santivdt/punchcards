7bb90f33782d77c9d2457c017b375fb791aab130
Santi van den Toorn <santi.vdt@gmail.com>
2024-07-12 14:47:38 +0200

    Accidently ignored this file before

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import ToastProvider from '@/components/toast-provider'
+import { GeistSans } from 'geist/font/sans'
+import { ThemeProvider } from 'next-themes'
+import { Next13NProgress } from 'nextjs13-progress'
+import { Metadata } from 'next'
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000'
+
+export const metadata: Metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: 'Punch it!',
+  description: 'Track your time with Punchcards',
+}
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang='en' className={GeistSans.className}>
+      <body className='antialiased dark:bg-black dark:text-neutral-300 h-screen flex flex-col'>
+        <ThemeProvider attribute='class'>
+          {children}
+          <Next13NProgress
+            color='#6D49FF'
+            height={3}
+            options={{ showSpinner: false }}
+          />
+          <ToastProvider />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
