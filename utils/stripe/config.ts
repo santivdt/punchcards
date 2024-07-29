6f7892709b049da7bdf9854c56b5ea99ee27c182
Maartje Snoeck Henkemans <maartje.snoeck@persgroep.net>
2024-07-29 10:17:09 +0200

    chore: Add Supabase and Stripe dependencies, and create utility files for Supabase and Stripe integration

---
+++ utils/stripe/config.ts
@@ -0,0 +1,17 @@
+import Stripe from 'stripe'
+
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY_LIVE ?? process.env.STRIPE_SECRET_KEY ?? '',
+  {
+    // https://github.com/stripe/stripe-node#configuration
+    // https://stripe.com/docs/api/versioning
+    apiVersion: '2024-06-20',
+    // Register this as an official Stripe plugin.
+    // https://stripe.com/docs/building-plugins#setappinfo
+    appInfo: {
+      name: 'PunchIT',
+      version: '0.0.0',
+      url: 'punchit.work',
+    },
+  }
+)
