b745ae3be257de2d115352e09afbfe84dc2317c7
Santi van den Toorn <santi.vdt@gmail.com>
2024-07-30 10:50:41 +0200

    Differenf formatting

--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,43 @@
-import { createClient } from "@/utils/supabase/middleware";
-import { type NextRequest, NextResponse } from "next/server";
+import { createClient } from '@/utils/supabase/middleware'
+import { type NextRequest, NextResponse } from 'next/server'
 
 const publicRoutes = [
-  "/",
-  "/about",
-  "/pricing",
-  "/contact",
-  "/login",
-  "/signup",
-  "/forgot-password",
-  "/reset-password",
-];
+  '/',
+  '/about',
+  '/pricing',
+  '/contact',
+  '/login',
+  '/signup',
+  '/forgot-password',
+  '/reset-password',
+]
 
 export async function middleware(request: NextRequest) {
-  const { supabase } = createClient(request);
+  const { supabase } = createClient(request)
 
-  if (request.nextUrl.pathname.startsWith("/auth")) {
-    return NextResponse.next();
+  if (request.nextUrl.pathname.startsWith('/auth')) {
+    return NextResponse.next()
   }
 
   const {
     data: { user },
-  } = await supabase.auth.getUser();
+  } = await supabase.auth.getUser()
 
   const isPublicRoute = publicRoutes.some(
     (route) =>
       request.nextUrl.pathname === route ||
-      request.nextUrl.pathname.startsWith(route),
-  );
+      request.nextUrl.pathname.startsWith(route)
+  )
 
   if (!user && !isPublicRoute) {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
-};
+}
