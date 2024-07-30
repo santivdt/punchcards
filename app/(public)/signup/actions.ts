33c84cb0cb317fd85dddc826cbf0eed4da43799a
Maartje Snoeck Henkemans <maartje.snoeck@persgroep.net>
2024-07-30 09:01:46 +0200

    oopsie

--- app/(public)/signup/actions.ts
+++ app/(public)/signup/actions.ts
@@ -1,26 +1,26 @@
-'use server'
+"use server";
 
-import { createClient } from '@/utils/supabase/server'
-import { redirect } from 'next/navigation'
-
-export const SignupWithOAuth = async (provider: 'google' | 'github') => {
-  const supabase = createClient()
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import { createOrRetrieveCustomer } from "@/utils/supabase/admin";
+export const SignupWithOAuth = async (provider: "google" | "github") => {
+  const supabase = createClient();
 
   const { data } = await supabase.auth.signInWithOAuth({
     provider: provider,
     options: {
       redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/callback/googlesignup`,
     },
-  })
+  });
 
-  return redirect(data.url!)
-}
+  return redirect(data.url!);
+};
 
 export const signUp = async (prevData: any, formData: FormData) => {
-  const email = formData.get('email') as string
-  const password = formData.get('password') as string
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
 
-  const supabase = createClient()
+  const supabase = createClient();
 
   const { data, error } = await supabase.auth.signUp({
     email,
@@ -28,9 +28,12 @@ export const signUp = async (prevData: any, formData: FormData) => {
     options: {
       emailRedirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/login`,
     },
-  })
-
-  console.log(data, error)
+  });
+  if (data?.user?.id) {
+    const bla = await createOrRetrieveCustomer({ email, uuid: data.user.id });
+    console.log(bla);
+  }
+  console.log(data, error);
 
   if (
     data &&
@@ -39,17 +42,17 @@ export const signUp = async (prevData: any, formData: FormData) => {
     data.user.identities.length === 0
   ) {
     return {
-      status: 'error',
+      status: "error",
       message:
-        'This email address is already associated with an existing account.',
-    }
+        "This email address is already associated with an existing account.",
+    };
   }
 
   if (error) {
-    return redirect('/signup?message=Could not complete sign-up of user')
+    return redirect("/signup?message=Could not complete sign-up of user");
   }
 
   return redirect(
-    '/login?message=Check your email to confirm your email address. You can sign in afterwards'
-  )
-}
+    "/login?message=Check your email to confirm your email address. You can sign in afterwards",
+  );
+};
--- utils/stripe/config.ts
+++ utils/stripe/config.ts
@@ -1,17 +1,18 @@
-import Stripe from 'stripe'
+import Stripe from "stripe";
 
 export const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY_LIVE ?? process.env.STRIPE_SECRET_KEY ?? '',
+  process.env.STRIPE_SECRET_KEY_LIVE ?? process.env.STRIPE_SECRET_KEY_TEST ??
+    "",
   {
     // https://github.com/stripe/stripe-node#configuration
     // https://stripe.com/docs/api/versioning
-    apiVersion: '2024-06-20',
+    apiVersion: "2024-06-20",
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
-      name: 'PunchIT',
-      version: '0.0.0',
-      url: 'punchit.work',
+      name: "PunchIT",
+      version: "0.0.0",
+      url: "punchit.work",
     },
-  }
-)
+  },
+);
