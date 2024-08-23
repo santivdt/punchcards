4f274973b7acea16803acb320fb2721fc839f2d8
Santi van den Toorn <santi.vdt@gmail.com>
2024-08-23 11:02:53 +0200

    Fix stupid mistakes

--- app/(public)/forgot-password/action.ts
+++ app/(public)/forgot-password/action.ts
@@ -1,47 +1,37 @@
-"use server";
-import { createClient as createSupabaseClient } from "@/utils/supabase/server";
-import { forgotSchema } from "./schema";
-import { redirect } from "next/navigation";
-import { headers } from "next/headers";
+'use server'
+import { createClient as createSupabaseClient } from '@/utils/supabase/server'
+import { forgotSchema } from './schema'
 
 export const forgotPassword = async (prevData: any, formData: FormData) => {
   const validatedFields = forgotSchema.safeParse({
-    email: formData.get("email"),
-  });
+    email: formData.get('email'),
+  })
 
   if (!validatedFields.success) {
     return {
-      status: "error",
-      errors: "Invalid email",
-    };
+      status: 'error',
+      errors: 'Invalid email',
+    }
   }
 
-  const supabase = createSupabaseClient();
-  // Get the headers object
-  const headersList = headers();
-  // Get the host and protocol from the headers
-  const host = headersList.get("host") || "";
-  const protocol = headersList.get("x-forwarded-proto") || "http";
-
-  // Construct the full URL
-  const baseUrl = `${protocol}://${host}`;
+  const supabase = createSupabaseClient()
 
   const { error } = await supabase.auth.resetPasswordForEmail(
     validatedFields.data.email,
     {
-      redirectTo: `${baseUrl}/reset-password`,
-    },
-  );
+      redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/reset-password`,
+    }
+  )
 
   if (error) {
     return {
-      status: "error",
+      status: 'error',
       errors: error.message,
-    };
+    }
   }
 
   return {
-    status: "success",
-    message: "Password reset link sent",
-  };
-};
+    status: 'success',
+    message: 'Password reset link sent',
+  }
+}
