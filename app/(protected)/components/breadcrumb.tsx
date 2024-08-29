5698fbfb1fd3c2b5b2adb25ee661d3992367379d
Santi van den Toorn <santi.vdt@gmail.com>
2024-08-29 17:11:52 +0200

    Improve breadcrump component

--- app/(protected)/components/breadcrumb.tsx
+++ app/(protected)/components/breadcrumb.tsx
@@ -1,75 +1,19 @@
 'use client'
-import { getCardFromId } from '@/app/(protected)/cards/actions'
-import { getClient } from '@/app/(protected)/clients/actions'
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
-  BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb'
 import { usePathname } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
 
-const replaceDashWithSpace = (arr: string[]) => {
-  return arr.map((str) => {
-    if (str.length < 15) {
-      return str.replace(/-/g, ' ')
-    }
-    return str
-  })
-}
-
-//TODO Ask Giel about this seems like a hack
-
-const Breadcrumbs = () => {
+const Breadcrumbs = ({ subPageName }: { subPageName?: string | null }) => {
   const pathname = usePathname()
-  const [crumbs, setCrumbs] = useState<string[] | null>([])
+  const paths = pathname.split('/').filter(Boolean)
 
-  useEffect(() => {
-    const fetchBreadcrumbs = async () => {
-      let pathSegments = pathname.split('/').filter(Boolean)
-
-      pathSegments = replaceDashWithSpace(pathSegments)
-
-      if (pathSegments.length < 2) {
-        return setCrumbs(pathSegments)
-      }
-
-      if (pathSegments[0] === 'clients') {
-        const crumbPromises = pathSegments.map(async (item) => {
-          if (item.length > 10) {
-            const { data: client } = await getClient(item)
-            return client ? client.name : item
-          }
-          return item
-        })
-
-        const resolvedCrumbs = (await Promise.all(crumbPromises)).filter(
-          (item): item is string => item !== null
-        )
-        setCrumbs(resolvedCrumbs)
-      } else if (pathSegments[0] === 'cards') {
-        const crumbPromises = pathSegments.map(async (item) => {
-          if (item.length > 10) {
-            const { data: card } = await getCardFromId(item)
-            return card ? `#${card.readable_id}` : item
-          }
-          return item
-        })
-
-        const resolvedCrumbs = (await Promise.all(crumbPromises)).filter(
-          (item): item is string => item !== null
-        )
-        setCrumbs(resolvedCrumbs)
-      }
-    }
-
-    fetchBreadcrumbs()
-  }, [pathname])
-
-  const pathLength = crumbs?.length
+  const page = paths[0] // page
+  const subPageId = paths[1] // id
 
   return (
     <Breadcrumb className='mt-2 mb-4'>
@@ -77,26 +21,31 @@ const Breadcrumbs = () => {
         <BreadcrumbItem>
           <BreadcrumbLink href='/dashboard'>Home</BreadcrumbLink>
         </BreadcrumbItem>
-        {crumbs && crumbs.length > 0 && <BreadcrumbSeparator />}
-        {crumbs &&
-          crumbs.slice(0, -1).map((item, index) => {
-            const pathSegmentsForUrl = pathname.split('/').filter(Boolean)
-            const url = `/${pathSegmentsForUrl.slice(0, index + 1).join('/')}`
 
-            return (
-              <React.Fragment key={index}>
-                <BreadcrumbItem>
-                  <BreadcrumbLink href={url}>{item}</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-              </React.Fragment>
-            )
-          })}
-        <BreadcrumbItem>
-          <BreadcrumbPage>
-            {pathLength && pathLength > 0 ? crumbs.slice(-1).toString() : null}
-          </BreadcrumbPage>
-        </BreadcrumbItem>
+        {page ? (
+          <>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href={`/${page}`} className='capitalize'>
+                {page}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </>
+        ) : null}
+
+        {subPageId ? (
+          <>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink
+                href={`/${page}/${subPageId}`}
+                className='capitalize'
+              >
+                {subPageName}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </>
+        ) : null}
       </BreadcrumbList>
     </Breadcrumb>
   )
