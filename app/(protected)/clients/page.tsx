8b4dbf9214d828fb50e4207b6d9c743b40cc615b
Santi van den Toorn <santi.vdt@gmail.com>
2024-08-30 11:51:34 +0200

    Refactor fetch

--- app/(protected)/clients/page.tsx
+++ app/(protected)/clients/page.tsx
@@ -1,20 +1,11 @@
-import { getClientsFromUserNew } from '@/app/(protected)/clients/actions'
+import { getClientsFromUser } from '@/app/(protected)/clients/actions'
 import Header from '@/components/header'
-import { PostgrestSingleResponse } from '@supabase/supabase-js'
 import InterMediateCreateClient from './intermediate-create-client'
 import { DataTable } from './table'
 import { columns } from './table/columns'
 
 const ClientsPage = async () => {
-  //TODO this is now because i am using try catch in the other file and using catch error message but not sure.
-  const response = await getClientsFromUserNew()
-
-  // TODO now i have to load like this where as i have generic loading in the other file?
-  if (!response) {
-    ;<p>Could not get clients</p>
-  }
-  // TODO this should be typed correctly?
-  const clients = (response as PostgrestSingleResponse<any>).data
+  const { data: clients } = await getClientsFromUser()
 
   return (
     <>
